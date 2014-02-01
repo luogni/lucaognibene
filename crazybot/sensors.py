e77278787c9f7da472cc76985a122784d4e0c8fe
Luca Ognibene <luca.ognibene@gmail.com>
2014-02-01 15:33:17 +0100

    different direction

--- crazybot/sensors.py
+++ crazybot/sensors.py
@@ -20,18 +20,22 @@ except:
     print "Can't open serial device"
     ser = None
 
+# m0 = m1 = 255
+# r = 0
+# ser.write('67,77,%d,%d,%d,20 s' % (int(m0), int(m1), 1 - int(r)))
+
 while 1:
     message, address = s.recvfrom(8192)
     (ts, y, z, r) = [m.strip() for m in message.split(',')]
     print ts, y, z, r
     power = int(max(0, min(TPOWER - float(z), TPOWER)) * (255.0 / TPOWER))  # 0  255
-    turn = max(-TTURN, min(float(y), TTURN)) / float(TTURN)  # -1  +1
+    turn = -1 * max(-TTURN, min(float(y), TTURN)) / float(TTURN)  # -1  +1
     m0 = power
     m1 = power
     if turn < 0:
         m1 *= (1 + turn)
     elif turn > 0:
         m0 *= (1 - turn)
-    print int(m0), int(m1), r
     if ser is not None:
-        ser.write('67,77,%d,%d,%d,20 s' % (int(m0), int(m1), 1 - int(r)))
+        print int(m0), int(m1), r
+        ser.write('67,77,%d,%d,%d,20 s' % (int(m0), int(m1), int(r)))
