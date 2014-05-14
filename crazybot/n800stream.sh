e2e476251a8a4ff1ac89edbad502ae6e8cee44a3
Luca Ognibene <luca.ognibene@gmail.com>
2014-05-14 14:58:11 +0200

    better quality and use mpeg4 to be compatible with aylook

--- crazybot/n800stream.sh
+++ crazybot/n800stream.sh
@@ -8,4 +8,5 @@ HEIGHT=288
 FPS=15
 PORT=1234
 
-gst-launch-0.10 v4l2src ! video/x-raw-yuv,width=$WIDTH,height=$HEIGHT,framerate=\(fraction\)$FPS/1 ! hantro4200enc stream-type=1 profile-and-level=1001 ! video/x-h263,framerate=\(fraction\)$FPS/1 ! tcpserversink protocol=gdp port=$PORT
+#while bin/true; do gst-launch-0.10 v4l2src ! video/x-raw-yuv,width=$WIDTH,height=$HEIGHT,framerate=\(fraction\)$FPS/1 ! hantro4200enc stream-type=1 profile-and-level=1007 bit-rate=515 ! video/x-h263,framerate=\(fraction\)$FPS/1 ! tcpserversink protocol=gdp port=$PORT; done
+while bin/true; do gst-launch-0.10 gconfv4l2src ! video/x-raw-yuv,width=$WIDTH,height=$HEIGHT,framerate=\(fraction\)$FPS/1 ! hantro4200enc stream-type=1 profile-and-level=245 bit-rate=515 ! video/mpeg,framerate=\(fraction\)$FPS/1 ! tcpserversink protocol=gdp port=$PORT; done
