3959ad70a7487ea60245226374cc18b198ace0bc
Luca Ognibene <luca.ognibene@gmail.com>
2017-02-22 07:43:25 +0100

    fix rust tab kbd

--- emacs/init.el
+++ emacs/init.el
@@ -222,5 +222,7 @@
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(add-hook 'rust-mode-hook
+  (lambda ()
+    (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)))
 (setq company-tooltip-align-annotations t)
